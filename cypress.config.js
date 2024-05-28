680c141f6b9fb05c236034d8fc973e29a368f9aa
betto <betto_beto@hotmail.com>
2024-05-28 20:08:21 -0300

    meu primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require("cypress");
+const {connect} = require('./cypress/support/mongo')
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      // implement node event listeners here
+      allureWriter(on, config);
+
+      const db = await connect()
+
+      on('task', {
+        async removeUser(email) {
+          const users = db.collection('users')
+          await users.deleteMany({email: email})
+          return null
+        },
+        async removeTask(taskName, emailUser) {
+          const users = db.collection('users')
+          const user = users.findOne({ email: emailUser})
+          const tasks = db.collection('tasks')
+          await tasks.deleteMany({name: taskName, user: user._id})
+          return null
+        },
+        async removeTaskLike(key) {
+          const tasks = db.collection('tasks')
+          await tasks.deleteMany({ name: {$regex: key} })
+          return null
+        }
+      })
+
+      return config
+    },
+    baseUrl: 'http://localhost:3333',
+    video: true
+  },
+});
